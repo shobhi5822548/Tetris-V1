94e2bb3b3fdc807dc26677f26805757ba1e494c1
shoban babu <35023345+shobhi5822548@users.noreply.github.com>
2024-10-20 13:35:58 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "shoban548bucket # Replace with your actual S3 bucket name
     key    = "eks/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
